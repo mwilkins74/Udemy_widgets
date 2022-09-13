7c8e9a3b3900d0712e6b1788090c2e37b8fc79f7
Mark <markwilkins86@gmail.com>
2022-09-13 16:16:29 -0400

    Convert Component Done

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,5 +1,8 @@
+// API Key AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM
+
 import React, { useState } from "react";
 import Dropdown from "./Dropdown";
+import Convert from "./Convert";
 
 const options = [
   {
@@ -13,6 +16,10 @@ const options = [
   {
     label: "Hindi",
     value: "hi",
+  },
+  {
+    label: "Dutch",
+    value: "nl",
   },
 ];
 
@@ -34,6 +41,9 @@ const Translate = () => {
         selected={language}
         onSelectedChange={setLanguage}
       />
+      <hr />
+      <h3 className="ui header">Output</h3>
+      <Convert language={language} text={text} />
     </div>
   );
 };
